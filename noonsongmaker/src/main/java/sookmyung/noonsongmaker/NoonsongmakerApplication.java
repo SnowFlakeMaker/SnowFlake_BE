7a787140e93d4ca499b73addf4838cf5e7ede809
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-19 15:55:20 +0900

    initial commit

---
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/NoonsongmakerApplication.java
@@ -0,0 +1,13 @@
+package sookmyung.noonsongmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoonsongmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoonsongmakerApplication.class, args);
+	}
+
+}
---
+++ noonsongmaker/src/test/java/sookmyung/noonsongmaker/NoonsongmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package sookmyung.noonsongmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoonsongmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
