0036d5970fac3eb1fb217d83fc2a10c828d94651
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-10 22:58:09 +0900

    Fix: signUp 내부 로직 구현
    1. requestDto의 email이 user 테이블에 존재하는지 검사 -> 존재 시 IllegalArgumentException 반환
      - custom 예외 특정 필요
    2. User.builder()...build()를 통해 User 객체 생성
    3. userRepository에 저장

--- noonsongmaker/src/main/java/Service/AuthService.java
+++ noonsongmaker/src/main/java/Service/AuthService.java
@@ -1,10 +1,10 @@
 package Service;
 
 import Dto.auth.SignupRequestDto;
+import Entity.User;
 import Repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,6 +18,15 @@ public class AuthService {
 
     @Transactional
     public void signUp(SignupRequestDto signupRequestDto) {
+        if (userRepository.existsByEmail(signupRequestDto.getEmail())) {
+            throw new IllegalArgumentException("Email already in use");
+        }
 
+        User user = User.builder()
+                .email(signupRequestDto.getEmail())
+                .password(passwordEncoder.encode(signupRequestDto.getPassword()))
+                .build();
+
+        userRepository.save(user);
     }
 }
