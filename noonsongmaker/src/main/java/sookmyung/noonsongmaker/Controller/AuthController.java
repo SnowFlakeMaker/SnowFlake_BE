937c7a60a45e16ec1b2fecdfc9ad2b1456052aef
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-12 21:39:50 +0900

    Feat: 이메일 전송 메소드 구현(/auth/send-email)
    - 요청 Body: MailSendDto(필드: [emailId])
    - emailService의 sendEmail 메소드 실행
    - 반환 Body: 성공 메세지만 반환, data는 null

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Controller/AuthController.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Controller/AuthController.java
@@ -1,7 +1,9 @@
 package sookmyung.noonsongmaker.Controller;
 
 import sookmyung.noonsongmaker.Dto.Response;
+import sookmyung.noonsongmaker.Dto.auth.MailSendDto;
 import sookmyung.noonsongmaker.Dto.auth.SignupRequestDto;
+import sookmyung.noonsongmaker.Dto.auth.VerificationRequestDto;
 import sookmyung.noonsongmaker.Service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sookmyung.noonsongmaker.Service.EmailService;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,10 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final EmailService emailService;
 
     @PostMapping("/signup")
     public ResponseEntity<Response<String>> signUp(@RequestBody SignupRequestDto signupRequestDto) {
-            authService.signUp(signupRequestDto);
+        authService.signUp(signupRequestDto);
         return ResponseEntity.ok(Response.buildResponse(null, "회원가입 성공"));
     }
+
+    @PostMapping("/send-email")
+    public ResponseEntity<Response<String>> sendVerificationCode(@RequestBody MailSendDto mailSendDto) {
+        emailService.sendEmail(mailSendDto);
+        return ResponseEntity.ok(Response.buildResponse(null, "인증 코드가 전송되었습니다."));
+    }
+
+
 }
