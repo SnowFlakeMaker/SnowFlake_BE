9dd3ba813f5825718f12c9eac5b4c530efedfea3
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-13 04:48:23 +0900

    Feat: 로그인 이후 회원 전용 api 요청에 대한 인가를 검증하는 필터 구현
    - doFilterInternational:
      - 요청으로부터 토큰을 받아 검증
      - 받은 토큰으로부터 email 추출 후, Userdetails 객체 생성

---
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/jwt/JwtProvider.java
@@ -0,0 +1,63 @@
+package sookmyung.noonsongmaker.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long accessTokenValidity = 1000 * 60 * 60; // 추후 시간 단축!
+    private final long refreshTokenValidity = 1000 * 60 * 60 * 24 * 7; // 7일
+
+    public String generateAccessToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenValidity))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshTokenValidity))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validateAccessToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("ACCESS_TOKEN".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
