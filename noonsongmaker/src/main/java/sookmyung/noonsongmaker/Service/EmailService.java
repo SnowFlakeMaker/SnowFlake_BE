62a8fa1a5bda0393cd0bf3fd161bc0d06c6180fd
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-12 21:36:27 +0900

    Feat: 이메일 전송 메소드 구현
    sendEmail():
    - 받은 emailId에 @sookmyung.ac.kr 붙여 수신자 주소 구성
    - generateCode() 통해 코드 생성
    - EMAIL_VERIFICATION_${emailId}를 key로 Redis에 코드 저장: 5분 후 만료
    - context에 생성한 코드 변수로 지정
    - templateEngine으로 htmlContent 생성
    - MimeMessage 및 MimeMessageHelper로 이메일 작성
    - JavaMailSender로 전송

---
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/EmailService.java
@@ -0,0 +1,51 @@
+package sookmyung.noonsongmaker.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import sookmyung.noonsongmaker.Dto.auth.MailSendDto;
+import sookmyung.noonsongmaker.Dto.auth.VerificationRequestDto;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
+    private final StringRedisTemplate redisTemplate;
+
+    private static final String EMAIL_PREFIX = "EMAIL_VERIFICATION_";
+    private static final long CODE_EXPIRE_TIME = 5L;
+
+    public void sendEmail(MailSendDto mailSendDto) {
+        try {
+            String email = mailSendDto.getEmailId() + "@sookmyung.ac.kr";
+            String verificationCode = generateCode();
+
+            redisTemplate.opsForValue().set(EMAIL_PREFIX + mailSendDto.getEmailId(), verificationCode, CODE_EXPIRE_TIME, TimeUnit.MINUTES);
+
+            Context context = new Context();
+            context.setVariable("verificationCode", verificationCode);
+
+            String htmlContent = templateEngine.process("email", context);
+
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+            mimeMessageHelper.setTo(email);
+            mimeMessageHelper.setSubject("눈송이메이커 회원 가입 이메일 인증 코드");
+            mimeMessageHelper.setText(htmlContent, true);
+            mailSender.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
