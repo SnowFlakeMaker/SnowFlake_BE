ab7ca413521bb0ef5e426eebaaa8fe350d139b17
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-13 04:13:42 +0900

    Feat: 로그인 메소드 구현
    - 성공 시 "로그인 성공" 메세지와 함께 로그인한 email id 반환
      - data: ["email": ${email}] 구조로 반환하면 더 좋을 듯
    - 쿠키 설정을 위해 HttpServletResponse를 매개변수로 받음

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Controller/AuthController.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Controller/AuthController.java
@@ -1,17 +1,19 @@
 package sookmyung.noonsongmaker.Controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import sookmyung.noonsongmaker.Dto.Response;
+import sookmyung.noonsongmaker.Dto.auth.LoginRequestDto;
 import sookmyung.noonsongmaker.Dto.auth.MailSendDto;
 import sookmyung.noonsongmaker.Dto.auth.SignupRequestDto;
 import sookmyung.noonsongmaker.Dto.auth.VerificationRequestDto;
-import sookmyung.noonsongmaker.Service.AuthService;
+import sookmyung.noonsongmaker.Service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sookmyung.noonsongmaker.Service.EmailService;
+import sookmyung.noonsongmaker.Service.auth.EmailService;
 
 @RestController
 @RequestMapping("/auth")
@@ -37,5 +39,11 @@ public class AuthController {
     public ResponseEntity<Response<Boolean>> verifyCode(@RequestBody VerificationRequestDto verificationRequestDto) {
         boolean isVerified = emailService.verifyCode(verificationRequestDto);
         return ResponseEntity.ok(Response.buildResponse(isVerified, isVerified ? "인증 성공" : "인증 실패"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response<String>> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        String email = authService.login(loginRequestDto, response);
+        return ResponseEntity.ok(Response.buildResponse(email, "로그인 성공"));
     }
 }
