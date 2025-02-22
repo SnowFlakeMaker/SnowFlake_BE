0bcaf8c1fb97be48a3b0cecf8a5c4818d94f25d3
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-22 21:27:53 +0900

    Feat: 튜플 업데이트 메소드 구현
    - 추후 확률 로직 적용 필요
    - 확률 로직 적용 시, 엔티티에 업데이트 메소드를 유지하는 게 적절할지 판단 필요

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Course.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Course.java
@@ -34,5 +34,18 @@ public class Course {
     private Short core3;
     private Short core4;
 
+    public void updateCourse(TimetableSubmitRequestDto dto) {
+        if (dto.getCoreCredits() != null) this.coreCredits = dto.getCoreCredits();
+        if (dto.getElectiveCredits() != null) this.electiveCredits = dto.getElectiveCredits();
 
+        if (dto.getRequiredDigital() != null) this.requiredDigital = dto.getRequiredDigital();
+        if (dto.getRequiredFuture() != null) this.requiredFuture = dto.getRequiredFuture();
+        if (dto.getRequiredEng() != null) this.requiredEng = dto.getRequiredEng();
+        if (dto.getRequiredLogic() != null) this.requiredLogic = dto.getRequiredLogic();
+
+        if (dto.getCore1() != null) this.core1 = dto.getCore1();
+        if (dto.getCore2() != null) this.core2 = dto.getCore2();
+        if (dto.getCore3() != null) this.core3 = dto.getCore3();
+        if (dto.getCore4() != null) this.core4 = dto.getCore4();
+    }
 }
