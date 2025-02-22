30cfb537cb04343a43bdabf0ec03621c118ea322
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-22 18:44:29 +0900

    Fix: enum 직렬화 메소드 구현

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Chapter.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Chapter.java
@@ -1,5 +1,7 @@
 package sookmyung.noonsongmaker.Entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Chapter {
     SEM_S_1, VAC_S_1, SEM_W_1, VAC_W_1,
     SEM_S_2, VAC_S_2, SEM_W_2, VAC_W_2,
@@ -8,5 +10,10 @@ public enum Chapter {
     SEM_S_5, VAC_S_5, SEM_W_5, VAC_W_5,
     SEM_S_6, VAC_S_6, SEM_W_6, VAC_W_6,
     SEM_S_7, VAC_S_7, SEM_W_7, VAC_W_7,
-    SEM_S_8, VAC_S_8, SEM_W_8, VAC_W_8
+    SEM_S_8, VAC_S_8, SEM_W_8, VAC_W_8;
+
+    @JsonValue
+    public String toJson() {
+        return this.name();
+    }
 }
