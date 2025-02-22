6cf58d93c8cde2409ce1e6d8fba9ef0c78347abb
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-22 18:53:30 +0900

    Feat: 수강해야 하는 교필 리스트업 메소드 작성

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/CourseService.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/CourseService.java
@@ -3,6 +3,7 @@ package sookmyung.noonsongmaker.Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sookmyung.noonsongmaker.Dto.course.CreditResponseDto;
+import sookmyung.noonsongmaker.Dto.course.RequiredResponseDto;
 import sookmyung.noonsongmaker.Entity.Course;
 import sookmyung.noonsongmaker.Entity.User;
 import sookmyung.noonsongmaker.Entity.UserProfile;
@@ -30,6 +31,18 @@ public class CourseService {
                 .majorType(userProfileResult.getMajorType())
                 .CurrentCoreCredits(courseResult.getCoreCredits())
                 .CurrentElectivesCredits(courseResult.getElectiveCredits())
+                .build();
+    }
+
+    public RequiredResponseDto getRequiredList(User user) {
+        Course courseResult = courseRepository.findByUser(user)
+                .orElseThrow(() -> new NoSuchElementException("수강 정보 소유자 없음"));
+
+        return RequiredResponseDto.builder()
+                .isRequiredDigital(courseResult.getIsRequiredDigital())
+                .isRequiredFuture(courseResult.getIsRequiredFuture())
+                .isRequiredEng(courseResult.getIsRequiredEng())
+                .isRequiredLogic(courseResult.getIsRequiredLogic())
                 .build();
     }
 }
