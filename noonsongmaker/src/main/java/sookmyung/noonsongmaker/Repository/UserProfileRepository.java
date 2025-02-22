6de7df62606bc55e584278673aa630a4f1d9e71b
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-22 18:42:47 +0900

    Feat: User 정보로 튜플 찾기 메소드 구현

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Repository/UserProfileRepository.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Repository/UserProfileRepository.java
@@ -2,12 +2,12 @@ package sookmyung.noonsongmaker.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sookmyung.noonsongmaker.Entity.Course;
 import sookmyung.noonsongmaker.Entity.User;
+import sookmyung.noonsongmaker.Entity.UserProfile;
 
 import java.util.Optional;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    Optional<Course> findByUser(User user);
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+    Optional<UserProfile> findByUser(User user);
 }
