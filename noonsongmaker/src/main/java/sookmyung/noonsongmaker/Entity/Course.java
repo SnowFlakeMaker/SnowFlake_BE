68980e31558301312aa2062f29abb852fa0ecb41
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-03-12 15:30:58 +0900

    Feat: 학점 취득 현황 업데이트 메소드 구현
    - 전공(Credits), 교필(Required), 교핵(Core) 단위로 묶어 구현

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Course.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Course.java
@@ -34,18 +34,22 @@ public class Course {
     private Short core3;
     private Short core4;
 
-    public void updateCourse(TimetableSubmitRequestDto dto) {
-        if (dto.getCoreCredits() != null) this.coreCredits = dto.getCoreCredits();
-        if (dto.getElectiveCredits() != null) this.electiveCredits = dto.getElectiveCredits();
+    public void updateCredits(Integer core, Integer elective) {
+        if (core != null) this.coreCredits = core;
+        if (elective != null) this.electiveCredits = elective;
+    }
 
-        if (dto.getRequiredDigital() != null) this.requiredDigital = dto.getRequiredDigital();
-        if (dto.getRequiredFuture() != null) this.requiredFuture = dto.getRequiredFuture();
-        if (dto.getRequiredEng() != null) this.requiredEng = dto.getRequiredEng();
-        if (dto.getRequiredLogic() != null) this.requiredLogic = dto.getRequiredLogic();
+    public void updateRequired(Boolean digital, Boolean future, Boolean eng, Boolean logic) {
+        if (digital != null) this.requiredDigital = digital;
+        if (future != null) this.requiredFuture = future;
+        if (eng != null) this.requiredEng = eng;
+        if (logic != null) this.requiredLogic = logic;
+    }
 
-        if (dto.getCore1() != null) this.core1 = dto.getCore1();
-        if (dto.getCore2() != null) this.core2 = dto.getCore2();
-        if (dto.getCore3() != null) this.core3 = dto.getCore3();
-        if (dto.getCore4() != null) this.core4 = dto.getCore4();
+    public void updateCore(Short core1, Short core2, Short core3, Short core4) {
+        if (core1 != null) this.core1 = core1;
+        if (core2 != null) this.core2 = core2;
+        if (core3 != null) this.core3 = core3;
+        if (core4 != null) this.core4 = core4;
     }
 }
