db16d6cb9d5ed327a64db46c94f40619fb394673
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-03-13 17:22:36 +0900

    Refactor: 전역 enum으로 분리

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/MajorType.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/MajorType.java
@@ -3,7 +3,7 @@ package sookmyung.noonsongmaker.Entity;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum MajorType {
-    DOUBLE_MAJOR, SUB_MAJOR, ADVANCED_MAJOR;
+    DOUBLE_MAJOR, SUB_MAJOR, ADVANCED_MAJOR, UNKNOWN;
 
     @JsonValue
     public String toJson() {
