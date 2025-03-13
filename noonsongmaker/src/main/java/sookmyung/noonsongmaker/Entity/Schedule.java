1db83e7dce829c50850ed95568709cc238310b5d
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-03-13 13:16:37 +0900

    Fix: 학기 및 방학여부 필드를 currentChapter로 통합, setter -> builder 변경

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Schedule.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Entity/Schedule.java
@@ -1,38 +1,40 @@
 package sookmyung.noonsongmaker.Entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "schedules")
 @Getter
-@Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Schedule {
-
     @Id
     @Column(name = "schedule_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long scheduleId;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "schedule_id")
-    private List<Plan> plans;
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "plan_id")
+    private Plan plan;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Column(nullable = false)
-    private int semester;
-
-    @Column(nullable = false)
-    private boolean isVacation;
+    private Chapter currentChapter;
 
     @Column(nullable = false)
     private int count;
+
+    public Schedule(Plan plan, User user, Chapter currentChapter, int count) {
+        this.plan = plan;
+        this.user = user;
+        this.currentChapter = currentChapter;
+        this.count = count;
+    }
 }
