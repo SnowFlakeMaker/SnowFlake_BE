e5bccc06618bd242f00a89d6c3a3fdfe955bf2e4
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2025-03-15 20:21:46 +0900

    Fix: ScheduleRepository 일부 수정, 오류 발생 부분 임시 주석 처리

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Repository/ScheduleRepository.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Repository/ScheduleRepository.java
@@ -2,14 +2,16 @@ package sookmyung.noonsongmaker.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sookmyung.noonsongmaker.Entity.Chapter;
 import sookmyung.noonsongmaker.Entity.Schedule;
+import sookmyung.noonsongmaker.Entity.User;
 
 import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    // 특정 유저의 학기 일정 조회 (isVacation = false)
-    List<Schedule> findByUserIdAndIsVacation(Long userId, boolean isVacation);
+    List<Schedule> findByUser(User user);
+
 }
 
