728a35e7ec549e20e618f4cb34029326884eec95
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-03-21 17:29:40 +0900

    Feat: 복수전공 / 부전공 과목 수강신청 비즈니스 로직 구현
    - 확률 정책 구현
    - 확률 정책과 랜덤 실수 크기 비교해 수강신청 성공 여부 반환

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/course/TimetableUpdatePolicy.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/course/TimetableUpdatePolicy.java
@@ -12,19 +12,33 @@ public class TimetableUpdatePolicy {
         this.chapter = chapter;
     }
 
+
     public boolean isMajorUpdatePossible() {
         return random.nextDouble() < getMajorSuccessRate();
     }
 
+    public boolean isDmUpdatePossible() { return random.nextDouble() < getDmSuccessRate(); }
+
     public boolean isLibUpdatePossible() {
         return random.nextDouble() < getLibSuccessRate();
     }
+
 
     private double getMajorSuccessRate() {
         return switch (chapter) {
             case SEM_S_1, SEM_W_1, SEM_S_2, SEM_W_2 -> 0.8;
             case SEM_S_3, SEM_W_3 -> 0.9;
             case SEM_S_4, SEM_W_4 -> 1.0;
+            default -> throw new IllegalStateException("Unexpected value: " + chapter);
+        };
+    }
+
+    private double getDmSuccessRate() {
+        return switch (chapter) {
+            case SEM_S_1, SEM_W_1 -> 0.4;
+            case SEM_S_2, SEM_W_2 -> 0.5;
+            case SEM_S_3, SEM_W_3 -> 0.6;
+            case SEM_S_4, SEM_W_4 -> 0.7;
             default -> throw new IllegalStateException("Unexpected value: " + chapter);
         };
     }
