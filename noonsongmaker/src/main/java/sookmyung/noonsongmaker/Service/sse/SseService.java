4189264b7feb09efb7b43485a5aefbaea0578dc7
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-03-21 15:58:58 +0900

    Feat: 단발성 이벤트 전송 메서드 구현
    - eventService.getOneTimeEvents 통해 이벤트명을 String으로 받아온 후 sse 전송
    - sendSse: sse emitter 조회 후 dto 및 전송 데이터 구성해 전송, IOException 발생 시 서버 로그 띄운 후 emitter 삭제

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/sse/SseService.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/sse/SseService.java
@@ -1,32 +1,52 @@
 package sookmyung.noonsongmaker.Service.sse;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import sookmyung.noonsongmaker.Dto.sse.SseEventResponseDto;
+import sookmyung.noonsongmaker.Dto.sse.SseEventType;
 import sookmyung.noonsongmaker.Entity.User;
+import sookmyung.noonsongmaker.Service.event.EventService;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SseService {
 
     private final SseEmitterRegistry emitterRegistry;
+    private final EventService eventService;
 
     public SseEmitter createEmitter(Long userId) {
         SseEmitter emitter = new SseEmitter(60L * 60L * 1000L);
         emitterRegistry.register(userId, emitter);
 
-        try {
-            emitter.send(SseEmitter.event().name("init").data("connected"));
-        } catch (IOException e) {
-            emitter.complete();
-            emitterRegistry.remove(userId);
-        }
+        sendSse(userId, SseEventType.INIT, "connected");
         return emitter;
     }
 
-    public void sendRegularEventsList(Long userId) {}
+    public void sendRegularEventsList(User user) {}
 
-    public void sendOneTimeEventList(Long userId) {}
+    public void sendOneTimeEventList(User user) {
+        List<String> oneTimeEventsName = eventService.getOneTimeEvents(user);
+
+        sendSse(user.getId(), SseEventType.ONETIME_EVENT, oneTimeEventsName);
+    }
+
+    private void sendSse(Long userId, SseEventType eventType, Object data) {
+        SseEmitter emitter = emitterRegistry.get(userId);
+        try {
+            SseEventResponseDto response = new SseEventResponseDto(data);
+            emitter.send(SseEmitter.event()
+                    .name(eventType.name().toLowerCase())
+                    .data(response));
+        } catch (IOException e) {
+            log.error("SSE 전송 실패. user_id {}: {}", userId, e.getMessage());
+            emitter.complete();
+            emitterRegistry.remove(userId);
+        }
+    }
 }
