bc3d1a73f2dbeba8d199a00dff56018ffbd90388
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-03-27 12:44:30 +0900

    Fix: 토큰 리프레쉬 요청 처리를 위한 토큰 유효성 검사 시 Blacklist 여부도 검사하는 로직 추가

--- noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/auth/RefreshTokenService.java
+++ noonsongmaker/src/main/java/sookmyung/noonsongmaker/Service/auth/RefreshTokenService.java
@@ -13,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class RefreshTokenService {
     private final StringRedisTemplate stringRedisTemplate;
     private final JwtProvider jwtProvider;
+    private static final String BLACKLIST_PREFIX = "BLACKLIST_";
 
     public void saveRefreshToken(String email, String refreshToken, long expiration) {
         stringRedisTemplate.opsForValue().set(email, refreshToken, expiration, TimeUnit.MILLISECONDS);
@@ -20,12 +21,13 @@ public class RefreshTokenService {
 
     public boolean validateRefreshToken(String email, String refreshToken) {
         String storedRefreshToken = stringRedisTemplate.opsForValue().get(email);
-        return storedRefreshToken != null && storedRefreshToken.equals(refreshToken);
+        String blacklist = stringRedisTemplate.opsForValue().get(BLACKLIST_PREFIX + refreshToken);
+        return storedRefreshToken != null && storedRefreshToken.equals(refreshToken) && blacklist == null;
     }
 
     public void invalidateRefreshToken(String email, String refreshToken) {
         stringRedisTemplate.delete(email);
         long expiration = jwtProvider.getExpiration(refreshToken);
-        stringRedisTemplate.opsForValue().set("BLACKLIST_" + refreshToken, "LOGOUT", expiration, TimeUnit.MILLISECONDS);
+        stringRedisTemplate.opsForValue().set(BLACKLIST_PREFIX + refreshToken, "LOGOUT", expiration, TimeUnit.MILLISECONDS);
     }
 }
